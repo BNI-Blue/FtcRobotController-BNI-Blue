0be690e713f904ff401b92c5aeae742541fcd359
bni-blue <bniblue@mtbethelchristian.org>
2024-09-09 17:22:18 -0400

    mecanum drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Blue17241/Drivetrains/MecanumDrive.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Blue17241.Drivetrains;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MecanumDrive {
+
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor rearLeftMotor;
+    public DcMotor rearRightMotor;
+
+    public static final double TICKS_PER_ROTATIONS = 386.3;
+
+    public LinearOpMode LinearOp = null;
+
+    //Default Constructor
+    public MecanumDrive(){
+    }
+
+    public void setLinearOp(LinearOpMode LinearOp) {this.LinearOp = LinearOp;}
+
+    public void setMotorRunModes(DcMotor.RunMode mode){
+        frontLeftMotor.setMode(mode);
+        frontRightMotor.setMode(mode);
+        rearLeftMotor.setMode(mode);
+        rearRightMotor.setMode(mode);
+    }
+
+    //Basic Drive Methods
+
+    public void stopMotors(){
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        rearLeftMotor.setPower(0);
+        rearRightMotor.setPower(0);
+    }
+
+    public void driveForward(double speed){
+        frontLeftMotor.setPower(speed);
+        frontRightMotor.setPower(speed);
+        rearLeftMotor.setPower(speed);
+        rearRightMotor.setPower(speed);
+    }
+
+    public void driveBack(double speed){
+        frontLeftMotor.setPower(-speed);
+        frontRightMotor.setPower(-speed);
+        rearLeftMotor.setPower(-speed);
+        rearRightMotor.setPower(-speed);
+    }
+
+    public void rotateLeft(double speed){
+        frontLeftMotor.setPower(-speed);
+        frontRightMotor.setPower(speed);
+        rearLeftMotor.setPower(-speed);
+        rearRightMotor.setPower(speed);
+    }
+
+    public void rotateRight(double speed){
+        frontLeftMotor.setPower(speed);
+        frontRightMotor.setPower(-speed);
+        rearLeftMotor.setPower(speed);
+        rearRightMotor.setPower(-speed);
+    }
+
+    public void strafeLeft(double speed){
+        frontLeftMotor.setPower(-speed);
+        frontRightMotor.setPower(speed);
+        rearLeftMotor.setPower(speed);
+        rearRightMotor.setPower(-speed);
+    }
+
+    public void strafeRight(double speed){
+        frontLeftMotor.setPower(speed);
+        frontRightMotor.setPower(-speed);
+        rearLeftMotor.setPower(-speed);
+        rearRightMotor.setPower(speed);
+    }
+
+    //Drive Methods w/ Encoders
+
+    public void driveForward(double speed, double rotations){
+        double ticks = rotations * TICKS_PER_ROTATIONS;
+        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        while ((Math.abs(frontLeftMotor.getCurrentPosition()) < ticks && LinearOp.opModeIsActive())){
+            driveForward(speed);
+            LinearOp.telemetry.addData("FL ticks ", frontLeftMotor.getCurrentPosition());
+            LinearOp.telemetry.addData("FR ticks ", frontRightMotor.getCurrentPosition());
+            LinearOp.telemetry.addData("RL ticks ", rearLeftMotor.getCurrentPosition());
+            LinearOp.telemetry.addData("RR ticks ", rearRightMotor.getCurrentPosition());
+        }
+    }
+
+    public void driveBack(double speed, double rotations){
+        double ticks = rotations * TICKS_PER_ROTATIONS;
+        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Blue17241/Drivetrains/TankDrive.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Blue17241.Drivetrains;
+
+public class TankDrive {
+}
