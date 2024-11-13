5ec32bcb593f37bddcd1fc0daac266e33bf7cb41
bni-blue <bniblue@mtbethelchristian.org>
2024-11-13 08:18:30 -0500

    Setting up Blue Programmer/Driver Bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Blue17241/Controls/Auto/Tester/TesterBlueAlliance.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Blue17241.Controls.Auto.Tester;
+
+import org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Blue17241.Controls.Auto.AutoMain;
+
+public abstract class TesterBlueAlliance extends TesterAutoMain {
+
+
+    public void blueNetSampleOne(){
+        Bot.strafeLeft(1, 3.4);
+        Bot.driveForward(1, 2);
+        Bot.rotateLeft(1, 3.7);
+        Bot.driveForward(1, 2.4);
+    }
+
+    public void blueNetSampleTwo(){
+        Bot.rotateRight(1, 1.8);
+        Bot.driveForward(1, 1.2);
+        Bot.rotateLeft(1, 1.5);
+        Bot.driveForward(1, 2);
+    }
+
+    public void blueNetSampleThree(){
+        Bot.driveBack(1, 2.8);
+        Bot.rotateRight(1, 1.8);
+        Bot.driveForward(1, 1.2);
+        Bot.rotateLeft(1, 1.5);
+        Bot.driveForward(1, 2);
+    }
+
+    public void blueNetPark(){
+
+    }
+}
